bfac3748426ecbf933022792753a572f57407025
Rishit1995 <rishitsanmukhani@gmail.com>
2015-08-28 23:45:45 +0530

    preparing final

---
+++ Q2/domain.cpp
@@ -0,0 +1,11 @@
+Zanskar Hostel:
+Default Gateway : 10.251.216.1
+10.10.1.2
+10.10.2.2
+
+Bharti:
+Default Gateway : 10.208.20.1
+nameserver 10.208.20.2
+nameserver 10.208.20.19
+nameserver 10.10.1.2
+nameserver 10.10.2.2
